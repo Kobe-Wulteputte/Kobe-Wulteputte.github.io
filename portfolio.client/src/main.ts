504097391357416fe99c7373435a07fa1014e82c
Kobe Wulteputte <kobe.wulteputte@orbid.be>
2023-03-17 13:38:59 +0100

    copy from old

--- portfolio.client/src/main.ts
+++ portfolio.client/src/main.ts
@@ -1,14 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import FontAwesomeIcon from './utilities/fontawesome-icons'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
